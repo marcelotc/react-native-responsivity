2b02302a41074ae4796fb3735bb030e416add1b4
Marcelo Cortes <marcelocortes@pop-os.localdomain>
2020-07-01 21:14:24 -0300

    responsive example

--- App.js
+++ App.js
@@ -5,23 +5,69 @@ import {
   StyleSheet
 } from 'react-native';
 
-import Header from './src/Header';
-import Boxes from './src/Boxes';
 
 const App = () => {
   return (
-
     <View style={styles.container}>
-      <Header></Header>
-      <Boxes></Boxes>
+      <View style={styles.top}>
+        <View style={styles.profileImage}></View>
+      </View>
+      <View style={styles.center}></View>
+      <View style={styles.bottom}>
+        <View style={styles.bottomItem}>
+          <View style={styles.bottomItemInner}></View>
+        </View>
+        <View style={styles.bottomItem}>
+          <View style={styles.bottomItemInner}></View>
+        </View>
+        <View style={styles.bottomItem}>
+          <View style={styles.bottomItemInner}></View>
+        </View>
+        <View style={styles.bottomItem}>
+          <View style={styles.bottomItemInner}></View>
+        </View>
+      </View>
     </View>
-
   );
 };
 
 const styles = StyleSheet.create({
   container: {
+    flex: 1
+  },
+  top: {
+    height: '45%',
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#98d2c1'
+  },
+  profileImage: {
+    width: 140,
+    height: 140,
+    borderRadius: 100,
+    borderWidth: 4,
+    borderColor: '#fff',
+    backgroundColor: '#eee'
+  },
+  center: {
+    height: '10%',
+    backgroundColor: '#7fbcac'
+  },
+  bottom: {
+    height: '45%',
+    backgroundColor: '#fff',
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    padding: 5
+  },
+  bottomItem: {
+    width: '50%',
+    height: '50%',
+    padding: 3
+  },
+  bottomItemInner: {
     flex: 1,
+    backgroundColor: '#292929'
   }
 })
 
